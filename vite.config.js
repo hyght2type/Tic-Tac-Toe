bee6c179c1a7036b8772a496a07fdbcb18d18d31
Peter Mercao <nicolasflammel@Peters-MacBook-Pro.local>
2026-05-27 09:21:39 -0700

    Fix vite config with explicit build input

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Tic-Tac-Toe/', 
+  base: '/Tic-Tac-Toe/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
 })
